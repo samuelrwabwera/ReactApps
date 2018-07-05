791a8871fb4a6f82ac15cf0fd85e3849ee880600
nalumagalydia <nalumagalydia@gmail.com>
2018-07-05 17:41:15 +0300

    updating

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,13 +1,25 @@
-import React from 'react';
-export class Button extends React.Component {
-    render() {
-        return (
-            <div id="myButtons" class="heads">
-                <button className="btn"><i className="fa fa-home"></i> Home</button>
-                <button id="menu" className="btn"><i className="fa fa-bars"></i> Menu</button>
-                
-            </div>
-        );
+import createElement from 'react';
+export class Button extends React.Component{
+render()
+{
+    return React.createElement('div', { id: "myButtons", className: "heads" }),
+    createElement('button', { className: "btn fa fa- home", "Home" }),
+    createElement('button', { id:"menu" className: "btn fa fa-bars",} "Menu")
+);
 
+
+
+
+
+
+
+class Hello extends React.Component {
+    render() {
+      return React.createElement('div', null, `Hello ${this.props.toWhat}`);
     }
-}
+  }
+  
+  ReactDOM.render(
+    React.createElement(Hello, {toWhat: 'World'}, null),
+    document.getElementById('root')
+  );
